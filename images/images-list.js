0d165ad2493309c33f7e20af86d5db4721307961
Adam Seidman <adamjseidman@gmail.com>
2025-10-13 21:44:21 -0400

    Add some pics and stuff

--- images/images-list.js
+++ images/images-list.js
@@ -1,4 +1,18 @@
 const galleryPhotos = {
-    
+    "bathAndLaundry": {
+        title: "Bath-Laundry Rooms",
+        images: [
+            "20200207_103903 (2).jpg",
+            "20200207_103907 (1).jpg",
+            "20200207_104225 (1).jpg",
+            "20200305_163420.jpg",
+            "20200305_163423.jpg",
+            "20200305_163437.jpg",
+            "20200305_163443.jpg",
+            "20200305_163449.jpg",
+            "20200305_163509.jpg",
+            "20200305_163516.jpg"
+        ]
+    }
 };
 Object.freeze(galleryPhotos);
